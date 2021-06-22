c24ab103d17c30bf14a17b0c876a6abd5af0d0f6
Igor Gonçalves <igordossantos38@gmail.com>
2021-06-22 14:28:29 -0300

    Backend working well, improvements necessaries

--- script.js
+++ script.js
@@ -5,8 +5,30 @@ function contar() {
     let res = window.document.querySelector('div#result');
     
     if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossível contar';
         window.alert('[ERRO] Faltam dados');
     } else {
-        res.innerHTML = 'Contando:';
+        res.innerHTML = 'Contando: <br>';
+        let i = Number(inicio.value);
+        let f = Number(fim.value);
+        let p = Number(passo.value);
+        if (p == 0) {
+            window.alert('Passo Inválido! Considerando passo 1');
+        }
+        
+        if (i < f) { //Contagem crescente 
+            for (let count = i; count <= f; count += p) {
+                res.innerHTML += ` ${count} \u{1F600}`;
+            } 
+        } else { //Contagem regressiva
+            for (let count = i; count >= f; count -= p) {
+                res.innerHTML += ` ${count} \u{1F600}`;
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
 }
+/* 
+Travamento quando colocado passo 0 
+Alinhamento vertical da cintagem
+*/
