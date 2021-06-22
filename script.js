dc4f6287d6906a652c79db252b2a155be1027334
Igor Gonçalves <igordossantos38@gmail.com>
2021-06-22 11:44:32 -0300

    Preparing the backend

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-window.alert,("Testando");
+function contar() {
+    let inicio = window.document.querySelector('input#txti');
+    let fim = window.document.querySelector('input#txtf');
+    let passo = window.document.querySelector('input#txtp');
+    let res = window.document.querySelector('div#result');
+    
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] Faltam dados');
+    } else {
+        res.innerHTML = 'Contando:';
+    }
+}
